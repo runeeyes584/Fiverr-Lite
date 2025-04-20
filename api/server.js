e61e18249c77c93cc06abbbe0269604059a454de
hungson100204 <hungson100204@gmail.com>
2025-04-20 14:24:53 +0700

    sửa servr.js

--- api/server.js
+++ api/server.js
@@ -1,28 +1,37 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js";
-import gigRoute from "./routes/gig.route.js";
-import orderRoute from "./routes/order.route.js";
-import conversationRoute from "./routes/conversation.route.js";
-import messageRoute from "./routes/message.route.js";
-import reviewRoute from "./routes/review.route.js";
-import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mysql from "mysql2";
+import authRoute from "./routes/auth.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import gigRoute from "./routes/gig.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
-mongoose.set("strictQuery", true);
 
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB!");
-  } catch (error) {
-    console.log(error);
+// Cấu hình kết nối MySQL
+const db = mysql.createConnection({
+  host: 'localhost',   // Địa chỉ máy chủ MySQL
+  user: 'root',        // Tên đăng nhập MySQL
+  password: '10022004',  // Mật khẩu MySQL
+  database: 'sys',  // Tên cơ sở dữ liệu bạn muốn kết nối
+  port: 3306,  // Cổng mặc định của MySQL
+  multipleStatements: true // Cho phép thực thi nhiều câu lệnh SQL
+});
+
+// Kết nối MySQL
+db.connect((err) => {
+  if (err) {
+    console.error('Lỗi kết nối MySQL:', err.stack);
+    return;
   }
-};
+  console.log('Đã kết nối MySQL với ID ' + db.threadId);
+});
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
@@ -44,6 +53,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8800, () => {
-  connect();
   console.log("Backend server is running!");
 });
